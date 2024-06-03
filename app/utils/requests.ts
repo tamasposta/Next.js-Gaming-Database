77c8326d3be28b083572946c004b3638c9989422
tamasposta <tamas.posta@outlook.com>
2024-06-03 16:39:25 +0200

    revalidation added to api requests

--- app/utils/requests.ts
+++ app/utils/requests.ts
@@ -3,7 +3,8 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 export const getGames = async () => {
   try {
     const res = await fetch(
-      `https://api.rawg.io/api/games?key=${API_KEY}&genres=action&ordering=-added&page_size=24&dates=2024-01-01,2024-12-31`
+      `https://api.rawg.io/api/games?key=${API_KEY}&genres=action&ordering=-added&page_size=24&dates=2024-01-01,2024-12-31`,
+      { next: { revalidate: 1 } }
     );
     const data = await res.json();
     return data.results;
@@ -16,7 +17,8 @@ export const getGames = async () => {
 export const getSearchedGames = async (query: string) => {
   try {
     const res = await fetch(
-      `https://api.rawg.io/api/games?key=${API_KEY}&search=${query}&page_size=36`
+      `https://api.rawg.io/api/games?key=${API_KEY}&search=${query}&page_size=36`,
+      { next: { revalidate: 1 } }
     );
     const data = await res.json();
     return data.results;
@@ -29,7 +31,8 @@ export const getSearchedGames = async (query: string) => {
 export const getGameDetails = async (slug: string): Promise<any | null> => {
   try {
     const res = await fetch(
-      `https://api.rawg.io/api/games/${slug}?key=${API_KEY}`
+      `https://api.rawg.io/api/games/${slug}?key=${API_KEY}`,
+      { next: { revalidate: 1 } }
     );
     const data = await res.json();
     return data;
@@ -42,7 +45,8 @@ export const getGameDetails = async (slug: string): Promise<any | null> => {
 export const getGameScreenshots = async (slug: string) => {
   try {
     const res = await fetch(
-      `https://api.rawg.io/api/games/${slug}/screenshots?key=${API_KEY}`
+      `https://api.rawg.io/api/games/${slug}/screenshots?key=${API_KEY}`,
+      { next: { revalidate: 1 } }
     );
     const data = await res.json();
     return data.results;
