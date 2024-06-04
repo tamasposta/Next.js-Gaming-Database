87dc0a8c0b6cdf8577969fe6baafdc062f4db144
tamasposta <tamas.posta@outlook.com>
2024-06-04 11:30:00 +0200

    fixing deployment

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default async function HomePage() {
       <div className="flex flex-col items-center mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 sm:my-20 max-sm:my-10">
         <h1 className="text-3xl text-primary mb-2">Trending Games</h1>
         <h2 className="text-xl text-secondary mb-10">Popular games in 2024</h2>
-        <h2 className="text-xl text-secondary mb-10">{games[0].name}</h2>
         <div>
           <div className="grid grid-cols:1 sm:grid-cols:2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-cols-max gap-6 items-start auto-rows-fr">
             {games?.map(
